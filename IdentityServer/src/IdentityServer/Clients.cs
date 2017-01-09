ff0f17c9369a070c574a7b5085b59e8173d0fa54
Roman Lumetsberger <roman.lumetsberger@outlook.com>
2017-01-09 21:00:13 +0100

    IdentityServer initial

---
+++ IdentityServer/src/IdentityServer/Clients.cs
@@ -0,0 +1,26 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+internal class Clients
+{
+    public static IEnumerable<Client> Get()
+    {
+        return new List<Client> {
+            new Client {
+                ClientId = "sfsClient",
+                ClientName = "SFS Application",
+                AllowedGrantTypes = GrantTypes.Implicit,
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    IdentityServerConstants.StandardScopes.Email,
+                    "role",
+                },
+                RedirectUris = new List<string> {"https://localhost:44330/signin-oidc"},
+                PostLogoutRedirectUris = new List<string> {"https://localhost:44330"}
+            }
+        };
+    }
+}
