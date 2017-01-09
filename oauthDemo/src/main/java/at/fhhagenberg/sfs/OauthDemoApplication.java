222ba4caf68def0da21317bc867f5b1b243e9a39
Wolfgang <wolfgang.lumetsberger@gmail.com>
2017-01-09 19:08:59 +0100

    added basic project structur

---
+++ oauthDemo/src/main/java/at/fhhagenberg/sfs/OauthDemoApplication.java
@@ -0,0 +1,12 @@
+package at.fhhagenberg.sfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthDemoApplication.class, args);
+	}
+}
