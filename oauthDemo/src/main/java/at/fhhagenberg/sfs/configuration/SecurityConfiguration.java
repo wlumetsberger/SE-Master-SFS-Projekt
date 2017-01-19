3e1fe4eae53160250227aec55ffa89fedd2936c1
Roman Lumetsberger <roman.lumetsberger@outlook.com>
2017-01-19 13:13:00 +0100

    added admintest

--- oauthDemo/src/main/java/at/fhhagenberg/sfs/configuration/SecurityConfiguration.java
+++ oauthDemo/src/main/java/at/fhhagenberg/sfs/configuration/SecurityConfiguration.java
@@ -41,7 +41,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.antMatcher("/**").authorizeRequests().antMatchers("/","/dist/**", "/vendor/**","/login**", "/landing").permitAll()
-                .antMatchers("/admin/**").hasRole("ADMIN") //protect admin using ROLE_ADMIN
+                //.antMatchers("/admin/**").hasRole("ADMIN") //protect admin using ROLE_ADMIN
                 .anyRequest()
                 .authenticated().and().exceptionHandling()
                 .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/")).and().logout()
---
+++ oauthDemo/src/main/java/at/fhhagenberg/sfs/controller/AdminTestController.java
@@ -0,0 +1,24 @@
+package at.fhhagenberg.sfs.controller;
+
+import at.fhhagenberg.sfs.model.UserContext;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+/**
+ * Created by Wolfgang on 09.01.2017.
+ */
+@Controller
+public class AdminTestController {
+
+    @RequestMapping("/admintest")
+    public String  home(final Authentication auth, final ModelMap model ){
+       model.clear();
+       model.addAttribute("data",auth);
+
+       return "/admintest";
+    }
+}
