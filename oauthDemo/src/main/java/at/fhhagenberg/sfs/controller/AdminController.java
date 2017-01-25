3e27cf62acb679566e1a6dbf808fbcfd9504742b
Wolfgang <wolfgang.lumetsberger@gmail.com>
2017-01-25 15:54:55 +0100

    - removed unused test.html - changed navigation to admin-template style - changed edit / delte buttons in list to icons - edit / new screen -> do not allow editing name of already created project - sessionstorage: added method to get entry by name, changed add method to store changed description on existing model

--- oauthDemo/src/main/java/at/fhhagenberg/sfs/controller/AdminController.java
+++ oauthDemo/src/main/java/at/fhhagenberg/sfs/controller/AdminController.java
@@ -5,6 +5,7 @@ import at.fhhagenberg.sfs.model.ProjectModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,40 +22,50 @@ public class AdminController extends AbstractController {
     @Autowired
     private SessionStorage storage;
 
-    @RequestMapping(value = "/admin/create", method = RequestMethod.GET)
-    public String home(final Model model) {
-
+    @RequestMapping(value = "/admin/add", method = RequestMethod.GET)
+    public String home(final ModelMap model) {
+        model.remove("projects");
+        model.remove("model");
+        model.remove("edit");
+        model.addAttribute("edit",false);
         model.addAttribute("model", new ProjectModel());
-
         return "create";
     }
 
     @RequestMapping(value = "/admin/list", method = RequestMethod.GET)
-    public String list(final Model model) {
-
-        model.addAttribute("model", new ProjectModel());
-
+    public String list(final ModelMap model) {
         return "listing";
     }
 
     @RequestMapping(value = "/admin/create", method = RequestMethod.POST)
     public String create(@ModelAttribute ProjectModel projectModel,
-                         final Model model) {
+                         final ModelMap model) {
+        storage.add(projectModel);
+        model.clear();
+        return "redirect:/admin/list";
+    }
 
-        model.addAttribute("model", projectModel);
-
+    @RequestMapping(value="/admin/edit", method = RequestMethod.POST)
+    public String edit(@RequestParam("name") String name, final ModelMap model) {
+        model.remove("model");
+        model.remove("projects");
+        model.remove("edit");
+        model.addAttribute("edit",true);
+        model.addAttribute("model",storage.getByKey(name));
         return "create";
     }
 
     @RequestMapping(value = "/admin/delete", method = RequestMethod.POST)
-    public String delete(@RequestParam("name") String name) {
+    public String delete(@RequestParam("name") String name,final ModelMap model) {
         storage.remove(name);
-
-        return "listing";
+        model.remove("edit");
+        model.clear();
+        return "redirect:/admin/list";
     }
 
     @ModelAttribute("projects")
     public List<ProjectModel> getProjectModels() {
+        List<ProjectModel> models = storage.getData();
         return storage.getData();
     }
 }
